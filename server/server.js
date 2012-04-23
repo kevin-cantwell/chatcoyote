4b9e89a7c54b8baafcc58cb4f5e3be5d4f11429c
Kevin Cantwell <kevin.cantwell@gmail.com>
2012-04-23 03:46:53 -0400

    Limit the messages in a chat to last 50.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 Meteor.startup(function () {
   Meteor.publish("messages", function(room_key) {
     return Messages.find({room_key: room_key}, {
-      fields: {room_key: false}
+      fields: {room_key: false},
+      sort: {timestamp: -1},
+      limit: 50
     });
   });
   // Disable synchronized client-side writes. Writes controlled via Meteor.call invokations.
