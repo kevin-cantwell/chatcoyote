2e70bc58af171192a13543795da4caacd2766af8
Kevin Cantwell <kevin.cantwell@gmail.com>
2012-05-09 01:20:24 -0400

    Remember users, even across rooms

--- client/startup.js
+++ client/startup.js
@@ -1,8 +1,10 @@
 Meteor.startup(function () {
   var room_name = Helpers.get_room_name();
-  var private_key = $.cookie("room:" + room_name);
+
+  var user_id = $.cookie("user_id");
+  var private_key = $.cookie("private_key");
   
-  Meteor.subscribe("usersessions", room_name, private_key, function() {
+  Meteor.subscribe("usersessions", room_name, user_id, private_key, function() {
     var session = UserSessions.findOne();
     Meteor.subscribe("users", session.room_id);
     Meteor.subscribe("messages", session.room_id);
@@ -11,7 +13,8 @@ Meteor.startup(function () {
     Session.set("room_id", session.room_id);
     Session.set("private_key", session.private_key);
 
-    $.cookie("room:" + room_name, session.private_key);
+    $.cookie("user_id", session.user_id);
+    $.cookie("private_key", session.private_key);
   });
 
   Session.set("last_msg_index", 0);
--- server/publish.js
+++ server/publish.js
@@ -1,27 +1,18 @@
 Rooms = new Meteor.Collection("rooms");
 
-Meteor.publish("usersessions", function(room_name, private_key) {
+Meteor.publish("usersessions", function(room_name, user_id, private_key) {
   var session_id, user_id;
   var room_id = Rooms.findsert({name: room_name});
-  var session = UserSessions.findOne({room_id: room_id, private_key: private_key});
-  if(session) {
-    session_id = session._id;
-    user_id = session.user_id;
+  if (Helpers.is_authentic(user_id, private_key)) {
+    session_id = UserSessions.findsert({user_id: user_id, room_id: room_id, private_key: private_key});
     var user = Users.findOne(user_id);
     if(user.rooms.indexOf(room_id) < 0) {
       user.rooms.push(room_id);
       Users.update(user._id, user);
     }
   } else {
-    user_id = Users.insert({
-      name: 'Anon' + new Date().getTime(),
-      rooms: [room_id]
-    });
-    session_id = UserSessions.insert({
-      user_id: user_id, 
-      room_id: room_id,
-      private_key: Meteor.uuid()
-    });
+    user_id = Users.insert({name: 'Anon' + new Date().getTime(), rooms: [room_id]});
+    session_id = UserSessions.insert({user_id: user_id, room_id: room_id, private_key: Meteor.uuid()});
   }
 
   // Only publish the single session for the current user
